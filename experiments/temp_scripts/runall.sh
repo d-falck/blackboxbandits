a9a1a84873466de5fa86cbc5202926484373bbc9
Damon Falck <damon.falck@worc.ox.ac.uk>
2022-05-07 17:54:20 +0000

    Rerun synth2a,b experiments

--- experiments/temp_scripts/runall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-python experiments/temp_scripts/meta1.py
-python experiments/temp_scripts/meta2.py
-python experiments/temp_scripts/meta3.py
-python experiments/temp_scripts/synth1.py
-python experiments/temp_scripts/synth2a.py
-python experiments/temp_scripts/synth2b.py
-python experiments/temp_scripts/synth3.py
