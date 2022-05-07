81a0aa53632fb8cffb91953fc5931732e36c9795
Damon Falck <damon.falck@worc.ox.ac.uk>
2022-05-07 17:32:54 +0000

    Run new experiments

--- experiments/temp_scripts/runall.sh
+++ experiments/temp_scripts/runall.sh
@@ -2,7 +2,7 @@
 python experiments/temp_scripts/meta1.py
 python experiments/temp_scripts/meta2.py
 python experiments/temp_scripts/meta3.py
-python experiments/temp_scripts/meta4.py
 python experiments/temp_scripts/synth1.py
-python experiments/temp_scripts/synth2.py
+python experiments/temp_scripts/synth2a.py
+python experiments/temp_scripts/synth2b.py
 python experiments/temp_scripts/synth3.py
